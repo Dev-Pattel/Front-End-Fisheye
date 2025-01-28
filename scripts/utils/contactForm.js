59851d23b581ae4a47e200cf197719ac3fa24245
Dev-Pattel <seigneurhuitre@gmail.com>
2025-01-28 15:34:49 +0100

    modification et mise en forme de la modale de contact, récupération des données utilisateurs à travers un console.log

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,18 +1,38 @@
+// DOM Champ de formulaire
+const form = document.querySelector("form")
+
+//Fonction pour faire apparaitre la modale de contact lors du clic de l'utilisateur
 async function displayModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "block";
     modal.setAttribute("aria-hidden", "false");
-   
   
     // Récupérer les données du photographe
     const { photographer } = await getPhotographerData();
   
     // Mettre à jour le nom dans le paragraphe sous le titre
     const photographerNameElement = document.getElementById("photographer-name");
-    photographerNameElement.textContent = photographer.name; // Insère le nom
+    photographerNameElement.textContent = photographer.name;
   }
-
+//Fonction pour fermer la modale de contact
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+//Fonction se déclenchant à l'envoi du formulaire par l'utilisateur en cliquant sur submit.
+//Ferme la modale après l'envoi et récupération des données envoyées à travers un console.log
+function SubmitContactForm() {
+  const firstName = document.getElementById("first-name").value
+  const lastName = document.getElementById("last-name").value
+  const email = document.getElementById("email").value
+  const userMessage = document.getElementById("votre-message").value
+  closeModal()
+
+  console.log(firstName, lastName, email, userMessage)
+}
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+  })
+
+  
